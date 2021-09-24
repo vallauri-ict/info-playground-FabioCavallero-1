c9179c3541e4a6eb1d234e0d214f2ffc368b1e0f
Stud. Fabio Cavallero <f.cavallero.1286@vallauri.net>
2021-09-24 10:51:37 +0200

    Concluso Esercizio Aereoplano

---
+++ Es.2 Aereoporto/Es.2 Aereoporto/Esercizi.sql
@@ -0,0 +1,6 @@
+﻿/*Le città da cui partono i voli per Roma, in ordine alfabetico*/
+SELECT DISTINCT v.CittaPart FROM Volo v WHERE v.CittaArr='Roma' ORDER BY v.CittaPart
+/*Città con aeroporto con numero di piste non noto*/
+SELECT a.Città FROM Aeroporto a WHERE a.NumPiste IS NULL
+/*Per ogni volo misto(sia merci che passaggeri): codice volo e dati di trasporto*/
+SELECT v.IdVolo, a.QtaMerci, a.NumPasseggeri FROM Aereo a, Volo v WHERE a.TipoAereo=v.TipoAereo AND a.QtaMerci>0 AND a.NumPasseggeri>0
